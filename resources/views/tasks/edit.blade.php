40c98604ea2346591ec2b15b579763a26382b919
miechan <neconeco_miesaku222@yahoo.co.jp>
2018-02-20 07:01:51 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,8 +8,8 @@
         <div class="col-xs-6">
             {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
                 <div class="form-group">
-                    {!! Form::label('title', 'ステータス:') !!}
-                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
 
                 <div class="form-group">
