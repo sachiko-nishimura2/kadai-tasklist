67865d6261bde8a903e6091f7860717447540a5f
miechan <neconeco_miesaku222@yahoo.co.jp>
2018-02-21 08:03:55 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
     <h1>タスク新規作成ページ</h1>
 
      <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
             {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
 
                 <div class="form-group">
