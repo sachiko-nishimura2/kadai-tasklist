cef7441afea3c5709fbe37d65152540d106ada5e
miechan <neconeco_miesaku222@yahoo.co.jp>
2018-02-21 02:06:30 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,8 @@
    <h1>id: {{ $tasks->id }} のタスク編集ページ</h1>
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-max">
+            <div class="col-sm-offset-2 col-sm-8">
             {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
@@ -20,6 +21,7 @@
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
             {!! Form::close() !!}
+            </div>
         </div>
     </div>
 
