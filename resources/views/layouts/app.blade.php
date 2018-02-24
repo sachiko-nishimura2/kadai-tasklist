ba23d5eaa096ad8f7d94fdd5dab6ee1961bf00f3
miechan <neconeco_miesaku222@yahoo.co.jp>
2018-02-24 08:33:11 +0000

    カラム

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,11 @@
             @include('commons.error_tasks')
 
             @yield('content')
-        </div>
 
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <div class="row">
+                <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+                {!! Form::model($message, ['route' => 'messages.store']) !!}
+            </div>
         </div>
-
     </body>
 </html>
