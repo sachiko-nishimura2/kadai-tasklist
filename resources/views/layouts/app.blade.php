dd84752bdee3c6e58d5b780772c80b2325a9774e
miechan <neconeco_miesaku222@yahoo.co.jp>
2018-02-24 08:39:02 +0000

    カラム

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
             <div class="row">
                 <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-                {!! Form::model($message, ['route' => 'messages.store']) !!}
+
             </div>
         </div>
     </body>
